059c90bdf705e4fa39502763dba618424386c445
nbarnes <nbarnes@gmail.com>
2019-04-03 19:47:54 -0700

    Bug fix in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "static#landing_page", as: 'landing_page'
-  get 'entries/search(/:content_search)(:tag_search)', to: 'entries#index', as: 'entries'
+  get 'entries/search(/:content_search)(:tag_search)', to: 'entries#index', as: 'entries_search'
   resources :entries
   get 'tags/', to: 'tags#index'
   get 'tags/:tag', to: 'tags#show', as: 'tag'
