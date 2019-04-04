62a3eb40b36d708d07e9cdc757ab4b7b804831cf
nbarnes <nbarnes@gmail.com>
2019-04-03 19:02:05 -0700

    Routing fix
    * 'entries/new' was matching against the search route (as a search for tag 'new'), so now the search route is namespaced

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "static#landing_page", as: 'landing_page'
-  get 'entries(/:content_search)(:tag_search)', to: 'entries#index', as: 'entries'
-  resources :entries, except: :index
+  get 'entries/search(/:content_search)(:tag_search)', to: 'entries#index', as: 'entries'
+  resources :entries
   get 'tags/', to: 'tags#index'
   get 'tags/:tag', to: 'tags#show', as: 'tag'
 end
